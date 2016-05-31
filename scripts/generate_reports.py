3bd79a73204192329ce4e983a58b10003914b2aa
Pablo Orviz <orviz@ifca.unican.es>
2016-05-31 15:24:43 +0000

    Work on templates

--- scripts/generate_reports.py
+++ scripts/generate_reports.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 
 import jenkins
 import jinja2
 from jinja2 import Template
 
-
-latex_jinja_env = jinja2.Environment(
-	block_start_string = '\BLOCK{',
-	block_end_string = '}',
-	variable_start_string = '\VAR{',
-	variable_end_string = '}',
-	comment_start_string = '\#{',
-	comment_end_string = '}',
-	line_statement_prefix = '%%',
-	line_comment_prefix = '%#',
-	trim_blocks = True,
-	autoescape = False,
-	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
-)
 
 code_style = {
     "pep8": {
@@ -40,26 +27,75 @@ products = {
                 {"name": "Functional and integration testing coverage", "id": "3656", "progress": "0"},
                 {"name": "GitBook documentation", "id": "3659", "progress": "53"}],
         },
+        #"repository": {"url": "https://github.com/indigo-dc/udocker"},
+        "repository": {"url": ""},
         "code_style": {"data": code_style["pep8"], "exceptions": None, "richness": "-"},
         "unittest": {"jenkins_job": "udocker-unittest", "graph": None, "data": []},
     }
 }
 
-for product in products.keys():
-    # jenkins
-    products[product]["unittest"]["graph"] = jenkins.save_cobertura_graph(
-        products[product]["unittest"]["jenkins_job"],
-        dest_dir="figs")
-    products[product]["unittest"]["data"] = jenkins.get_cobertura_data(
-        products[product]["unittest"]["jenkins_job"])
 
-    # latex
-    template = latex_jinja_env.get_template("report.tex")
-    print(template.render(
-        product=product,
-        period="Apr-May 2016",
-        tasks=products[product]["tasks"],
-        code_style=products[product]["code_style"],
-        unit_test=products[product]["unittest"],
-    ))
+def parse_args():
+    parser = argparse.ArgumentParser(description="Generate INDIGO-DataCloud SQA reports")
+    parser.add_argument('template',
+			metavar="FILE",
+			type=str,
+			help="LaTeX template location.")
+    parser.add_argument('--report',
+			metavar="FILE",
+                        #default="indigo_sqa.tex",
+			type=str,
+			help="LaTeX report file location.")
+    return parser.parse_args()
 
+
+def load_jinja(fname):
+    if os.path.isabs(fname):
+        load_dir = os.path.dirname(fname)
+    else:
+        load_dir = os.path.dirname(os.path.abspath(fname))
+
+    return jinja2.Environment(
+            block_start_string = '\BLOCK{',
+            block_end_string = '}',
+            variable_start_string = '\VAR{',
+            variable_end_string = '}',
+            comment_start_string = '\#{',
+            comment_end_string = '}',
+            line_statement_prefix = '%%',
+            line_comment_prefix = '%#',
+            trim_blocks = True,
+            autoescape = False,
+            loader = jinja2.FileSystemLoader(load_dir)
+    )
+
+
+def main(fname, output=None):
+    latex_jinja_env = load_jinja(fname)
+    for product in products.keys():
+        # jenkins
+        products[product]["unittest"]["graph"] = jenkins.save_cobertura_graph(
+            products[product]["unittest"]["jenkins_job"],
+            dest_dir="figs")
+        products[product]["unittest"]["data"] = jenkins.get_cobertura_data(
+            products[product]["unittest"]["jenkins_job"])
+
+        # latex
+        template = latex_jinja_env.get_template(os.path.basename(fname))
+        r = template.render(
+                product=product,
+                period="Apr-May 2016",
+                tasks=products[product]["tasks"],
+                repository=products[product]["repository"],
+                code_style=products[product]["code_style"],
+                unit_test=products[product]["unittest"],
+        )
+        if output:
+            open(args.report, 'w').write(r)
+        else:
+            print(r)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args.template, output=args.report)
