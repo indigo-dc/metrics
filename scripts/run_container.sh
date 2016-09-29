9a76092392b46c8457a58f5255c048fe9b1bf841
Pablo Orviz <orviz@ifca.unican.es>
2016-09-29 10:37:32 +0000

    Fix metrics launch script

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -11,13 +11,21 @@ done
 # File containing the configuration variables 
 source /root/.sqa/sqarc
 
+# Checks
+#1 metrics container
+#2 variables
+[[ $REPO_PATH ]] && [[ $MYSQL_ROOT_PASSWORD ]] && [[ $MYSQL_USER ]] && [[ $MYSQL_PASSWORD ]]
+[[ $? -eq 1 ]] && echo "Not all the needed variables (REPO_PATH/MYSQL_ROOT_PASSWORD/MYSQL_USER/MYSQL_PASSWORD) are defined" && exit 1
+
+
 Echo "Launching MetricsDB.."
-docker run --name metricsdb -v /metricsfs:/var/lib/mysql \
-			     -v $REPO_PATH/scripts/initdb.d:/docker-entrypoint-initdb.d \
+docker run --name metricsdb -p 3306:3306 \
+                             -v /metricsfs:/var/lib/mysql \
+			     -v $REPO_PATH/docker/metrics/initdb.d:/docker-entrypoint-initdb.d \
 			     -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
 			     -e MYSQL_USER=$MYSQL_USER \
 			     -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
-			     -d orviz/indigo-metricsdb
+			     -d mariadb:latest
 
 Echo "Launching metrics manager.."
 docker run --name metrics --link metricsdb:mysql \
