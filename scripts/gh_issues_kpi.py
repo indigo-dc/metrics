2d6f5248cf1d2bd39cda1f4985947840ea72ea75
Pablo Orviz <orviz@ifca.unican.es>
2016-12-14 13:21:24 +0100

    Added defect stats

--- scripts/gh_issues_kpi.py
+++ scripts/gh_issues_kpi.py
@@ -6,10 +6,27 @@ user = raw_input("GitHub User: ")
 password = getpass.getpass()
 
 gh = login(user, password)
-query = "is:issue user:indigo-dc is:closed label:bug closed:<2016-11-01"
+query = "is:issue user:indigo-dc is:closed label:bug closed:<%s"
 
-def get_defect_removal_resolution_time():
+def get_defect_no(to_date):
+    return len([issue for issue in gh.search_issues(query % to_date)])
+
+def get_defect_rejected_no(to_date):
+    query = ("is:issue user:indigo-dc is:closed label:bug closed:<%s "
+             "label:invalid")
+    return len([issue for issue in gh.search_issues(query % to_date)])
+
+def get_defect_removal_resolution_time(to_date):
     l = []
-    for issue in gh.search_issues(query):
+    for issue in gh.search_issues(query % to_date):
         l.append((issue.issue.closed_at - issue.issue.created_at).days)
     return sum(l)/float(len(l))
+
+print("## 1st release ##")
+to_date = "2016-11-01"
+print("* Number of defects: %s" % get_defect_no(to_date))
+print("* Number of defects rejected: %s" % get_defect_rejected_no(to_date))
+print("* KPI Defect Removal Resolution Time: %s"
+      % get_defect_removal_resolution_time(to_date))
+print("* KPI Rejected Defect Ratio: %s:%s"
+      % (get_defect_rejected_no(to_date), get_defect_no(to_date)))
