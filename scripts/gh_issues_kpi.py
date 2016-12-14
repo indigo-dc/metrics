1b119b52781b0cfdf3c877a8c8b15739466b70bc
Pablo Orviz <orviz@ifca.unican.es>
2016-12-14 16:45:58 +0100

    fix total defects gathering && add new kpi

--- scripts/gh_issues_kpi.py
+++ scripts/gh_issues_kpi.py
@@ -6,19 +6,27 @@ user = raw_input("GitHub User: ")
 password = getpass.getpass()
 
 gh = login(user, password)
-query = "is:issue user:indigo-dc is:closed label:bug closed:<%s"
 
 def get_defect_no(to_date):
-    return len([issue for issue in gh.search_issues(query % to_date)])
+    query = "is:issue user:indigo-dc label:bug created:<%s" % to_date
+    return len([issue for issue in gh.search_issues(query)])
+
+def get_defect_closed_no(to_date):
+    query = "is:issue user:indigo-dc label:bug created:<%s is:closed" % to_date
+    return len([issue for issue in gh.search_issues(query)])
 
 def get_defect_rejected_no(to_date):
-    query = ("is:issue user:indigo-dc is:closed label:bug closed:<%s "
-             "label:invalid")
-    return len([issue for issue in gh.search_issues(query % to_date)])
+    query = ("is:issue user:indigo-dc label:bug created:<%s "
+             "label:invalid") % to_date
+    return len([issue for issue in gh.search_issues(query)])
+
+def get_defect_removal_efficiency(to_date):
+    return (float(get_defect_closed_no(to_date))/get_defect_no(to_date))*100
 
 def get_defect_removal_resolution_time(to_date):
+    query = "is:issue user:indigo-dc label:bug created:<%s is:closed" % to_date
     l = []
-    for issue in gh.search_issues(query % to_date):
+    for issue in gh.search_issues(query):
         l.append((issue.issue.closed_at - issue.issue.created_at).days)
     return sum(l)/float(len(l))
 
@@ -26,6 +34,7 @@ print("## 1st release ##")
 to_date = "2016-11-01"
 print("* Number of defects: %s" % get_defect_no(to_date))
 print("* Number of defects rejected: %s" % get_defect_rejected_no(to_date))
+print("* KPI Defect Removal Efficiency: %s" % get_defect_removal_efficiency(to_date))
 print("* KPI Defect Removal Resolution Time: %s"
       % get_defect_removal_resolution_time(to_date))
 print("* KPI Rejected Defect Ratio: %s:%s"
