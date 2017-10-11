bed6ab9ab1a578757e6a6981784c19ef8d3af8fb
Pablo Orviz <orviz@ifca.unican.es>
2017-10-11 13:29:38 +0200

    indigo-2 release

--- scripts/gh_issues_kpi.py
+++ scripts/gh_issues_kpi.py
@@ -54,17 +54,42 @@ def get_defect_removal_resolution_time(to_date):
         l.append((issue.issue.closed_at - issue.issue.created_at).days)
     return sum(l)/float(len(l))
 
+## 1st release
 print("## 1st release ##")
-major_release = "2016-08-08"
-first_update = "2016-09-19"
-second_update = "2016-10-11"
-third_update = "2016-10-28"
-reporting_period = "2016-11-01"
+INDIGO1_major_release = "2016-08-08"
+INDIGO1_first_update = "2016-09-19"
+INDIGO1_second_update = "2016-10-11"
+INDIGO1_third_update = "2016-10-28"
+INDIGO1_four_update = "2016-11-16"
+INDIGO1_five_update = "2016-12-05"
+INDIGO1_six_update = "2016-12-30"
+INDIGO1_seven_update = "2017-02-03"
+INDIGO1_eight_update = "2017-03-01"
+INDIGO1_nine_update = "2017-03-24"
+## 2nd release
+print("## 2nd release ##")
+INDIGO2_major_release = "2017-03-31"
+INDIGO2_first_update = "2017-06-01"
+INDIGO2_second_update = "2017-07-07"
+INDIGO2_third_update = "2017-08-05"
+INDIGO2_four_update = "2017-09-01"
+INDIGO2_five_update = "2017-09-30"
+reporting_period = "2017-10-01"
 
-for d in [("major release", major_release),
-          ("first update", first_update, major_release),
-          ("second update", second_update, first_update),
-          ("third update", third_update, second_update)]:
+for d in [
+        ("INDIGO1_four_update", INDIGO1_four_update, INDIGO1_third_update),
+        ("INDIGO1_five_update", INDIGO1_five_update, INDIGO1_four_update),
+        ("INDIGO1_six_update", INDIGO1_six_update, INDIGO1_five_update),
+        ("INDIGO1_seven_update", INDIGO1_seven_update, INDIGO1_five_update),
+        ("INDIGO1_eight_update", INDIGO1_eight_update, INDIGO1_six_update),
+        ("INDIGO1_nine_update", INDIGO1_nine_update, INDIGO1_eight_update),
+        ("INDIGO2_major_release", INDIGO2_major_release, INDIGO1_nine_update),
+        ("INDIGO2_first_update", INDIGO2_first_update, INDIGO2_major_release),
+        ("INDIGO2_major_update", INDIGO2_second_update, INDIGO2_first_update),
+        ("INDIGO2_major_update", INDIGO2_third_update, INDIGO2_second_update),
+        ("INDIGO2_major_update", INDIGO2_four_update, INDIGO2_third_update),
+        ("INDIGO2_major_update", INDIGO2_five_update, INDIGO2_four_update),
+    ]:
     print("- %s -" % d[0])
     print("* Number of defects: %s"
           % get_defect_no(*d[1:]))
